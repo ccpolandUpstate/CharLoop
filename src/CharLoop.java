a9e99998a53cc3417e6ba90cc318906636ed3f24
Chase Poland <chase.poland@gmail.com>
2022-03-17 03:13:19 -0400

    Nicer Looking

--- src/CharLoop.java
+++ src/CharLoop.java
@@ -10,59 +10,59 @@ import java.util.Scanner;
 public class CharLoop
 {
     // THE METHOD FOR DETECTING THE # OF VOWELS 
-    public static int vowels(String vCount)
+    public static int vowels(String str)
     {
         int countVowels = 0;
-        for (int i = 0; i < vCount.length(); i++) {
-            char vChar = vCount.charAt(i);
-            if (vChar == 'a' || vChar =='e' || vChar == 'i' || vChar == 'o' || vChar == 'u') {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
                 countVowels ++;
             }
         }
         return countVowels;
     }
     // THE METHOD FOR DETECTING THE # OF UPPER CASE LETTERS
-    public static int upperCase(String upperCaseCounter)
+    public static int upperCase(String str)
     {
         int countUpperCase = 0; 
-        for (int i = 0; i < upperCaseCounter.length(); i++) {
-            if (upperCaseCounter.charAt(i) >= 'A' && upperCaseCounter.charAt(i) <= 'Z') {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
                 countUpperCase++;
             }
         }
         return countUpperCase;
     }
     // THE METHOD FOR DETECTING THE # OF DIGITS 
-    public static int digitCount(String digitCounter)
+    public static int digitCount(String str)
     {
         int countDigits = 0;
-        for (int i = 0; i < digitCounter.length(); i++) {
-            if (digitCounter.charAt(i) >= '0' && digitCounter.charAt(i) <= '9') {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
                 countDigits++;
             }
         }
         return countDigits;
     }
     // THE METHOD FOR DETECTING THE # OF WHITESPACES
-    public static int whiteSpace(String whiteSpaceCounter) 
+    public static int whiteSpace(String str) 
     {
         int countWhiteSpace = 0;
-        for (int i = 0; i < whiteSpaceCounter.length(); i++) {
-            if (whiteSpaceCounter.charAt(i) == ' ') {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
                 countWhiteSpace++;
             }
         }
         return countWhiteSpace; 
     }
         // METHOD FOR DETERMING WHICH VOWEL SHOWS UP MOST
-    public static void whichVowel(String whatVowel) {
+    public static void whichVowel(String str) {
         char output_C = ' '; int c_max = 0, count = 0;
         char c;
-        for (int i=0; i < whatVowel.length(); i++){
-            c = whatVowel.charAt(i);
-            if (c == 'a' || c =='e' || c == 'i' || c == 'o' || c == 'u') {
-                for (int j=0; j < whatVowel.length(); j++){
-                    if (whatVowel.charAt(j) == c)
+        for (int i = 0; i < str.length(); i++){
+            c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                for (int j = 0; j < str.length(); j++){
+                    if (str.charAt(j) == c)
                     count++;
                 }
             }
@@ -71,7 +71,7 @@ public class CharLoop
                 output_C = c;
             }
             count = 0;
-            whatVowel = whatVowel.replace(c,' ');
+            str = str.replace(c,' ');
         }
         if (c_max > 0)
         System.out.println("vowel " + output_C + " occurs the most = " + c_max);
@@ -80,16 +80,16 @@ public class CharLoop
         return;
     }
     // THE METHOD FOR DETECTING REPEATED CHARACTERS
-    public static void repeatedC(String repeatedCharacters) {
+    public static void repeatedC(String str) {
         StringBuilder f = new StringBuilder();
         char c;
-        for (int i = 1; i < repeatedCharacters.length(); i++) {
-            c = repeatedCharacters.charAt(i);
-            if (repeatedCharacters.charAt(i) == repeatedCharacters.charAt(i-1)) {
+        for (int i = 1; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (str.charAt(i) == str.charAt(i-1)) {
                 f.append(c).append(" ");
             }
         }
-        if (f.length() > 1)
+        if (f.length() > 0)
         System.out.print("repeated characters: " + f);
         else
         System.out.println("no repeating characters");
@@ -100,10 +100,10 @@ public class CharLoop
         try (// Scanner Class & Initalization
         Scanner scan = new Scanner(System.in)) {
             String _userInput_ = scan.nextLine();
-
-             // Vowel Count: Pulled from Line: 12: Method: Vowel
             String vowels = _userInput_.toLowerCase();
-            System.out.println("vowels = " + vowels(vowels));
+
+            // Vowel Count: Pulled from Line: 12: Method: Vowel
+            System.out.println("vowels = " + vowels(vowels)); // .toLowerCase so that it accounts for all vowels.
 
             // Uppercase Count: Pulled from Line: 24: Method: upperCase
             System.out.println("upper = " + upperCase(_userInput_));
